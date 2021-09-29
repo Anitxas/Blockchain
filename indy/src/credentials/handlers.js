2f057be2e2522d82f390ced9050a5b8b5313226d
Anitxas <61308272+Anitxas@users.noreply.github.com>
2021-09-29 11:08:48 +0200

    Update handlers.js

--- indy/src/credentials/handlers.js
+++ indy/src/credentials/handlers.js
@@ -1,6 +1,8 @@
 'use strict';
 const indy = require('../../index.js');
 
+//Un request se hace para asegurarse de la identidad del receptor, y se deberá identificar usando la credencial(credential).
+
 exports.request = function (message) {
     return indy.credentials.acceptRequest(message.origin, message.message);
 };
